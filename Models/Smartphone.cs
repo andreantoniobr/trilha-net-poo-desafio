1d97119634fee1356a435d863875a8af878636f3
André Antônio <andreantoniobr@hotmail.com>
2023-10-31 20:36:50 -0300

    feat: add iphone and nokia smartphones

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,13 +2,18 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string _modelo;
+        private string _imei;
+        private int _memoria;
 
-        public Smartphone(string numero)
+        public string Numero { get; set; }
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            _modelo = modelo;            
+            _imei = imei;
+            _memoria = memoria;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone(numero: "999999999", modelo: "Modelo 01", imei: "111111111", memoria: 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "888888888", modelo: "Modelo 02", imei: "222222222", memoria: 512);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
